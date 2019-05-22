4d49d595b1d1045c0d565754abfc828fa2bbaeca
Tiago Avila <tiago.avila09@gmail.com>
2019-05-22 14:28:33 -0300

    fix firebase key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyClWu1sSwEh4igzDlx5AVbg_MBSM3D5pBw",
-    authDomain: "pwa-example-d5bf6.firebaseapp.com",
-    databaseURL: "https://pwa-example-d5bf6.firebaseio.com",
-    projectId: "pwa-example-d5bf6",
-    storageBucket: "pwa-example-d5bf6.appspot.com",
-    messagingSenderId: "147295303239"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
